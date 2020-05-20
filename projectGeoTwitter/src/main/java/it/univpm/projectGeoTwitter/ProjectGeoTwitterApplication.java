4d51568337916a40bdf69a380adceafc9d8bd5f2
Francesco Paoli Leonardi <s1087147@studenti.univpm.it>
2020-05-20 15:58:16 +0200

    Test push progetto

--- projectGeoTwitter/src/main/java/it/univpm/projectGeoTwitter/ProjectGeoTwitterApplication.java
+++ projectGeoTwitter/src/main/java/it/univpm/projectGeoTwitter/ProjectGeoTwitterApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectGeoTwitterApplication {
 
 	public static void main(String[] args) {
+		System.out.print("Hello");
 		SpringApplication.run(ProjectGeoTwitterApplication.class, args);
 	}
 
