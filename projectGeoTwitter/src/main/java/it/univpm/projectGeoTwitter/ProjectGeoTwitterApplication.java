90d1f2bc1ec6322dbb08ef4b4fa191f8bf55a88c
devoz <devoz@DESKTOP-7N0FOOK.station>
2020-05-20 15:47:30 +0200

    Base del progetto

---
+++ projectGeoTwitter/src/main/java/it/univpm/projectGeoTwitter/ProjectGeoTwitterApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.projectGeoTwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGeoTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGeoTwitterApplication.class, args);
+	}
+
+}
